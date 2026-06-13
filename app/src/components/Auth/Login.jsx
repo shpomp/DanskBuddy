cd7ab7de4989ae36aeb8b6c932fd96f2e5fb20ef
irynalopatina <lopatina.iryna@gmail.com>
2026-06-13 23:49:18 +0200

    feat: add login page

---
+++ app/src/components/Auth/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+function Login() {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleLogin(event) {
+    event.preventDefault();
+    setError("");
+
+    const result = login(email, password);
+
+    if (result.success) {
+      navigate("/browse");
+    } else {
+      setError(result.error);
+    }
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+
+        {error && <p>{error}</p>}
+
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="Email address"
+          required
+        />
+
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder="Password"
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </section>
+  );
+}
+
+export default Login;
