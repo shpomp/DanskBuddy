4a29ff49316540219fc396112bf788caff591ae0
shpomp <karolina.urnieziute@gmail.com>
2026-06-14 12:58:18 +0200

    [chore/typescript] - LevelBadge.jsx to tsx

--- app/src/components/Shared/LevelBadge.tsx
+++ app/src/components/Shared/LevelBadge.tsx
@@ -1,4 +1,10 @@
-export default function LevelBadge({ level }) {
+type Level = "A1" | "A2" | "B1" | "B2" | "C1" | "C2" | "native";
+
+interface Props {
+  level: Level;
+}
+
+export default function LevelBadge({ level }: Props) {
   let className = "badge-gray";
 
   if (level === "A1" || level === "A2") className = "badge-green";
