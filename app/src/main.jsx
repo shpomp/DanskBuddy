ab9297d194066a7419ffebef424da902207a3190
Jyoti <switjyoti26~@gmail.com>
2026-06-17 11:21:14 +0200

    Add messages routes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { initStorage } from "./utils/initStorage";
-import { AppProvider } from "./context/AppContext";
+import { initStorage } from "./utils/initStorage.js";
+import { AppProvider } from "./context/AppContext.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 import HomePage from "./components/HomePage/HomePage.jsx";
 import TestPage from "./components/TestPage/TestPage.jsx";
+import MessagesPage from "./components/Messages/MessagesPage.jsx";
+import ChatWindow from "./components/Messages/ChatWindow.jsx";
 import "./main.css";
 initStorage();
 const router = createBrowserRouter([
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
       {
         path: "nested",
         element: <TestPage />,
+      },
+      {
+        path: "messages",
+        element: <MessagesPage />,
+      },
+      {
+        path: "messages/:userId",
+        element: <ChatWindow />,
       },
     ],
   },
