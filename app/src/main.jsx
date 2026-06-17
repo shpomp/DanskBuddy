9758e69cbf7088e57ebb552686c958824fb226aa
Jyoti <switjyoti26~@gmail.com>
2026-06-17 12:01:39 +0200

    Remove chat window route until chat feature is added

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -7,8 +7,8 @@ import Layout from "./components/Layout/Layout.jsx";
 import HomePage from "./components/HomePage/HomePage.jsx";
 import TestPage from "./components/TestPage/TestPage.jsx";
 import MessagesPage from "./components/Messages/MessagesPage.jsx";
-import ChatWindow from "./components/Messages/ChatWindow.jsx";
 import Login from "./components/Auth/Login.jsx";
+
 import "./main.css";
 initStorage();
 const router = createBrowserRouter([
@@ -28,11 +28,8 @@ const router = createBrowserRouter([
         path: "messages",
         element: <MessagesPage />,
       },
+
       {
-        path: "messages/:userId",
-        element: <ChatWindow />,
-      },
-       {
         path: "/login",
         element: <Login />,
       },
