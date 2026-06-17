6556a64583e4b2708a7e7867880c57ad6809a151
Rufaisashah <157133453+Rufaisashah@users.noreply.github.com>
2026-06-17 14:13:18 +0200

    change logo to reference logo

--- app/src/components/HomePage/HomePage.jsx
+++ app/src/components/HomePage/HomePage.jsx
@@ -1,10 +1,10 @@
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[70vh] text-center gap-4">
-      <h1 className="text-4xl font-bold text-rose-900">
+      <h1 className="text-4xl font-bold text-red-400">
         The Hygge Way to Danish
       </h1>
-      <p className="text-black-500 max-w-md">
+      <p className="text-black-900 max-w-md">
         Connect with Danish speakers and learners to practice your language
         skills together.
       </p>
